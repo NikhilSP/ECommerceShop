08e2432e12722a247527700331d800f9a401f494
Nikhil Pundir <npundir@cerebre.io>
2024-12-12 06:58:23 -0600

    feat : added initial catalog api

---
+++ src/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+
+namespace CatalogAPI.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, double Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = request.Name,
+            Category = request.Category,
+            Description = request.Description,
+            ImageFile = request.ImageFile,
+            Price = request.Price
+        };
+
+        // Save to Database
+
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
